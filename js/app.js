27fcc77e86423a03f5e180bed7079e6286cc5e80
Blanclang <blanclang@163.com>
2016-08-20 21:22:51 +0800

    bai:登录页 首页 资料编辑

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
 angular.module('starter', ['starter.controller', 'ngAnimate', 'ngTouch', 'ngFileUpload'])
 .config(['$routeProvider', 
 	function($routeProvider){
-		$routeProvider.when('/login', {
+		$routeProvider
+		.when('/login', {
 			templateUrl: 'template/login.html'
+			/*controller: 'homeController'*/
+		}).when('/register', {
+			templateUrl: 'template/register.html'
+		}).when('/getBackPsd', {
+			templateUrl: 'template/getBackPsd.html'
+		}).when('/userInfoEdit', {
+			templateUrl: 'template/userInfoEdit.html'
 		});
-
 		//当找不到链接页面后跳转首页
 		$routeProvider.otherwise('/login');
 	}
