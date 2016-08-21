b08c4979fc988c909a5171a1deeb7d60e7b39ef2
Blanclang <blanclang@163.com>
2016-08-22 07:51:30 +0800

    bai:页面基本完成 细节要调

--- js/app.js
+++ js/app.js
@@ -11,6 +11,24 @@ angular.module('starter', ['starter.controller', 'ngAnimate', 'ngTouch', 'ngFile
 			templateUrl: 'template/getBackPsd.html'
 		}).when('/userInfoEdit', {
 			templateUrl: 'template/userInfoEdit.html'
+		}).when('/sexEdit', {
+			templateUrl: 'template/sexEdit.html'
+		}).when('/resumeEdit', {
+			templateUrl: 'template/resumeEdit.html'
+		}).when('/schoolSelEdit', {
+			templateUrl: 'template/schoolSelEdit.html'
+		}).when('/districtSelEdit', {
+			templateUrl: 'template/districtSelEdit.html'
+		}).when('/districtInfoEdit', {
+			templateUrl: 'template/districtInfoEdit.html'
+		}).when('/userNameEdit', {
+			templateUrl: 'template/userNameEdit.html'
+		}).when('/myWorkAxis', {
+			templateUrl: 'template/myWorkAxis.html'
+		}).when('/jobDetails', {
+			templateUrl: 'template/jobDetails.html'
+		}).when('/myCollection', {
+			templateUrl: 'template/myCollection.html'
 		});
 		//当找不到链接页面后跳转首页
 		$routeProvider.otherwise('/login');
