d398ef5bef3b301ec44f67988fdbaf3c9397ee60
weierstrass-cui <244169731@qq.com>
2016-10-13 14:21:32 +0800

    保存简历BUG

--- js/commonController.js
+++ js/commonController.js
@@ -191,6 +191,8 @@ var mainCtrl = '';
 					if( myResume && myResume.isChange == '1' ){
 						$scope.commonFn.confirmMsg(null, '您的简历已经修改，是否立即保存？', function(){
 							last.scope.fn.saveResume(true);
+						}, function(){
+							$storage.removeLocalStorage('SQZ_resume');
 						});
 					}
 				}
